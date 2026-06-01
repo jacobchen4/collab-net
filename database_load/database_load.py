af1ea38b64956f3c6bfe0fa99489c6c0735e703b
adrodvaz04 <isyla.adrian@gmail.com>
2026-06-01 23:12:50 +1000

    feat: implement coauthorship edge queries

--- database_load/database_load.py
+++ database_load/database_load.py
@@ -13,12 +13,14 @@ graph = pyneoinstance.Neo4jInstance(creds['uri'],creds['user'],creds['password']
     
 def getPublicationsForAuthor(author):
     return graph.execute_read_query(
-        queries['cypher']['get_publications_for_author'],
+        query=queries['cypher']['get_publications_for_author'],
+        database=creds['database'],
         parameters={'author': author})
 
 def getAuthorsForPublication(pub_key):
     return graph.execute_read_query(
-        queries['cypher']['get_authors_for_publication'],
+        query=queries['cypher']['get_authors_for_publication'],
+        database=creds['database'],
         parameters={
             'pub_key': pub_key
         })
@@ -29,10 +31,44 @@ def getAllAuthors():
 def getAllPublications():
     return graph.execute_read_query(queries['cypher']['get_all_authors'])
 
+
+# Returns a series of objects ({pub_key, coauthors}) that represents the given author's
+# coauthors and their corresponding publications
 def getCoauthorsForAuthor(author):
-    publications = getPublicationsForAuthor(author)
-    print(publications)
+    publications = getPublicationsForAuthor(author)['p']
     
+    # helper method to get unique coauthors given a publication
+    def getCoauthoredPublications(publication) :
+        coauthors = set()
+        authors = getAuthorsForPublication(publication['pub_key'])
+        # filter authors to remove selected author
+        filtered_authors = authors[authors['a'].apply(lambda x: x['author']) != author]
+        # add each co-author to coauthors set
+        for _, row in filtered_authors.iterrows():
+            coauthors.add(row['a']['author'])
+        return coauthors
+    
+    return publications.apply(lambda p: {
+        'pub_key': p['pub_key'],
+        'coauthors': getCoauthoredPublications(p)
+        })
+
+def addCoauthorsForAuthor(author):
+    # get publication-coauthor objects for given author
+    pubCoauthorsObjs = getCoauthorsForAuthor(author)
+    for coauthorsObj in pubCoauthorsObjs:
+        pub_key = coauthorsObj['pub_key']
+        # for each coauthor in each publication, add the corresponding coauthorship edge
+        for coauthor in coauthorsObj['coauthors']:
+            graph.execute_write_query(
+                query=queries['cypher']['add_coauthorship_edge'],
+                database=creds['database'],
+                parameters={
+                    'author': author,
+                    'pub_key': pub_key,
+                    'coauthor': coauthor
+                })
+
 if __name__ == "__main__":
     # test
-    print(getAllAuthors())
+    addCoauthorsForAuthor('Dongju Lee')
